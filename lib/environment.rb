8f4af99baebd03ca53a8cfe11d505087c307e268
Cassandra <cparisi1290@gmail.com>
2021-11-30 17:13:23 -0500

    require pry and cli file

---
+++ lib/environment.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'pry'
+
+require_relative "vending_machine_cli/version"
+require_relative "vending_machine_cli/cli"
+
+module VendingMachineCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
